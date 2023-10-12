3cda74f6afdc6476a82eef1bb3eba06cf47a281b
gabumwaingo <gabumwaingo@gmail.com>
2023-10-11 18:12:17 -0700

    Task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,12 +8,12 @@ import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int = 0, max_delay: int = 10) -> list[float]:
     """
     Takes two ints n and max_delay
     spawns wait_random n times and stores delays in list
     """
-    delay = []
+    delay: list[float] = []
     tasks = [wait_random(max_delay) for _ in range(n)]
     delay = await asyncio.gather(*tasks)
     return delay
