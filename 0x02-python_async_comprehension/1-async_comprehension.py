84676d444c187c3890b50f19a01836a1a3581329
gabumwaingo <gabumwaingo@gmail.com>
2023-10-11 19:46:24 -0700

    Task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Async comprenhesion gen """
+import asyncio
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+        Generate numbers with async gen
+    """
+    return ([i async for i in async_generator()])
